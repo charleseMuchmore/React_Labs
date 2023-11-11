5cbb8e4f434483acfba56cd3c4490a54830351a8
Charlese Muchmore <charlymuchmore@gmail.com>
2023-11-10 18:05:46 -0800

    NavBar

--- lab5/blog/src/components/NavBar.js
+++ lab5/blog/src/components/NavBar.js
@@ -1,5 +1,40 @@
+import { useContext, useState } from 'react';
+import UserContext from '../context/user';
+import LoginForm from '../components/LoginForm';
+
 function NavBar() {
-    return <div>NavBar</div>
+    const { user, resetUser } = useContext(UserContext);
+    const [showLogin, setShowLogin] = useState(false);
+
+    const handleClick = () => {
+        if (showLogin === false && user === false) {
+            setShowLogin(true);
+        } else if (showLogin === true) {
+            setShowLogin(false);
+        } else {
+            resetUser();
+        }
+    };
+
+    const handleLoginSubmit = () => {
+        setShowLogin(false);
+    }
+
+    return (
+    <div>
+        <div href="../pages/Home.js">Logo</div>
+        {user && 
+        <button onClick={handleClick}>login</button>}
+        {user && user.id &&
+        <div>
+            <a href="#">add post</a>
+            <a href="#">update profile</a>
+            <button onClick={handleClick}>logout</button>
+        </div>}
+        {showLogin === true &&
+        <LoginForm onSubmit={handleLoginSubmit}/>}
+    </div>
+    )
 }
 
 export default NavBar;
