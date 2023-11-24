111a3c1e1b50f9c39085d344185d3a770b95e09d
Charlese Muchmore <charlymuchmore@gmail.com>
2023-11-24 15:10:10 -0800

    refixing header

--- lab6/blog/src/components/Header.js
+++ lab6/blog/src/components/Header.js
@@ -2,7 +2,7 @@ import bookImage from '../images/booksimage.jpg'
 import './Header.css'
 
 function Header() {
-    return <div className="image-style"><img src={bookImage} alt="header" /></div> //<img src={bookImage} alt="header" />
+    return <div className="image-style"><img alt=""/></div> //<img src={bookImage} alt="header" />
 }
 
 export default Header;
